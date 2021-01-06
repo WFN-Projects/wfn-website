2d70958d33bea9840e8de3bb77689930d9054628
Jack <jackepeplinski@gmail.com>
2021-01-06 13:15:27 -0700

    Basic text.

---
+++ src/theme/globalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+export default createGlobalStyles
+    * {
+        font-family: 'Archivo', sans-serif;
+    }
+`;
+
+export default GlobalStyle;
---
+++ src/theme/theme.js
@@ -0,0 +1,10 @@
+export default {
+    colors: {
+        white: white,
+        black: #333333,
+        pink: #C54E9E, //use this until we start using gradients
+    },
+    fontSize: {
+
+    },
+}
