c021ca8239e90746efd8180394cf1accbc5a1186
Nicholas C <chongnicke@gmail.com>
2021-01-11 13:35:07 -0500

    Test Use about page to test routes and gatsby Link

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,10 +3,11 @@ import styled, { ThemeProvider } from 'styled-components';
 import { graphql, useStaticQuery } from "gatsby";
 import Header from '../components/header'
 import React from 'react'
+import { gsap } from 'gsap';
 import "../css/background-image.css";
 import "../css/global.css";
 
-const AboutPage = () => {
+const Test = () => {
   const data = useStaticQuery(graphql`
     query AboutImage {
       image: file(relativePath: {eq: "test.jpg"}) {
@@ -21,9 +22,24 @@ const AboutPage = () => {
   `)
   return (
     <BackgroundImage className="indexPage" fluid={data.image.childImageSharp.fluid}>
-      <Header/>
+      <Header textColor="black"/>
     </BackgroundImage>
   )
+}
+
+class AboutPage extends React.Component {
+  componentDidMount() {
+    var tl = gsap.timeline()
+    tl.to("#test", {duration: 1, opacity: "100%"})
+  }
+
+  render() {
+    return (
+      <div id="test">
+        <Test/>
+      </div>
+    )
+  }
 }
 
 export default AboutPage
