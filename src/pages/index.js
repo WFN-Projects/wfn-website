c8e72b4e2010465938134579260a77207757d786
Nicholas C <chongnicke@gmail.com>
2021-01-11 13:27:58 -0500

    test inverted button

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import { graphql, useStaticQuery } from "gatsby";
 import "../css/background-image.css";
 import "../css/global.css";
 import Button from '../components/button'
+import ButtonInverted from '../components/buttonInverted'
 import Header from '../components/header'
 
 const theme = {
@@ -63,7 +64,7 @@ const IndexPage = () => {
   return (
     <ThemeProvider theme={theme}>
       <BackgroundImage className="indexPage" fluid={data.image.childImageSharp.fluid}>
-        <Header/>
+        <Header textColor="white"/>
         <Banner>
           <H1>Western Founders Network</H1>
           <H2>The largest technology, business, and entrepreneurship club at Western University.</H2>
@@ -73,7 +74,10 @@ const IndexPage = () => {
         <div>
           <H1>Who We Are 🚀</H1>
           <P>Western Founders Network is a community of <b>creatives and innovators</b>. Wielding the forces of <b>technology, entrepreneurship, and business,</b> our members convert long-term goals into practical and creative plans for action. Our club enables those with a <b>drive</b>, alongside a community of like-minded peers, to have an impact by opening the door to new skillsets and frameworks of thoughts, such as through our educational and flagship conferences.</P>
-          <Button text="Our Initiatives"/>
+          <div style={{display: "flex", alignItems: "center", gap: "20px"}}>
+            <Button text="Our Initiatives"/>
+            <ButtonInverted text="Meet the Team"/>
+          </div>
         </div>
         {/* <iframe 
           width="2000" 
