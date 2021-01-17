b34d5972f5e288a46d45c6f963cdcd77808759a3
Nicholas-Chong <chongnicke@gmail.com>
2021-01-17 12:31:11 -0500

    Make entire button area clickable

--- src/components/buttonInverted.js
+++ src/components/buttonInverted.js
@@ -4,13 +4,13 @@ import { Link } from 'gatsby'
 
 const ButtonInverted = (props) => {
   return (
-    <Outer>
-      <Inner>
-        <Link to={props.link ? props.link : "/"} style={{textDecoration: "none", color: "black", margin: "auto"}}>
+    <Link to={props.link ? props.link : "/"} style={{textDecoration: "none"}}>
+      <Outer>
+        <Inner>
           <Text>{props.text}</Text>
-        </Link>
-      </Inner>
-    </Outer>
+        </Inner>
+      </Outer>
+    </Link>
   )
 }
 
@@ -48,6 +48,10 @@ const Inner = styled.div`
 
 const Text = styled.div`
 	background: linear-gradient(160.67deg, #FFE9B3 -3.1%, #C54E9E 77.92%);
-	-webkit-background-clip: text;
-	-webkit-text-fill-color: transparent;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
 `;
