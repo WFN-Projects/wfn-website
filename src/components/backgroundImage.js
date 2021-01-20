3b810f9d3dc496fd023a26f4a98ff7d3821a67b1
Nicholas-Chong <chongnicke@gmail.com>
2021-01-20 15:19:48 -0500

    Create custom background image component Fixes safari white screen flickering and includes header by default

---
+++ src/components/backgroundImage.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import styled from 'styled-components'
+import Img from 'gatsby-image'
+import Header from '../components/header'
+
+const BackgroundImage = (props) => {
+  return(
+    <div>
+      <CustomImage fluid={props.fluid}/>
+      <Overlay>
+        <Header textColor="white" />
+        <TextBoxContainer>
+          <TextBox>
+            {props.children}
+          </TextBox>
+        </TextBoxContainer>
+      </Overlay>
+    </div>
+  )
+}
+
+export default BackgroundImage
+
+const CustomImage = styled(Img)`
+  width: 100%;
+  height: 120vh;
+  margin-top: -1vh;
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  z-index: 1;
+`;
+
+const TextBoxContainer = styled.div`
+  position: absolute;
+  top: 0;
+  z-index: -1;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex: 0 0 100%;
+  color: white;
+`;
+
+const TextBox = styled.div`
+  margin: auto;
+  text-align: center;
+`;
