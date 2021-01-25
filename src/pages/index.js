e139dfdd9d2c300749782021b318b9e6276e2eb7
Jack <jackepeplinski@gmail.com>
2021-01-24 18:11:15 -0700

    Start on conditional rendering.

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,42 +37,42 @@ const IndexPage = () => {
         <H1 center white>Western Founders Network</H1>
         <H2>The largest technology, business, and entrepreneurship club at Western University.</H2>
       </BackgroundImage>
-      <BackgroundShading>
-        <Content>
-        <WhoWeAre>
-          <WhoWeAreText>
-            <H1>Who We Are 🚀</H1>
-            <div className="whoWeAreTextContainer">
-              <P>Western Founders Network is a community of <b>creatives and innovators</b>. Wielding the forces of <b>technology, entrepreneurship, and business,</b> our members convert long-term goals into practical and creative plans for action. Our club enables those with a <b>drive</b>, alongside a community of like-minded peers, to have an impact by opening the door to new skillsets and frameworks of thoughts, such as through our educational and flagship conferences.</P>
-            </div>
-            <WhoWeAreButtons>
-              <Button text="Our Initiatives" />
-              <ButtonInverted text="Meet the Team" />
-            </WhoWeAreButtons>
-          </WhoWeAreText>
-          <WhoWeAreVideo>
-            <div className="container">
-              <iframe
-                className="responsive-iframe"
-                src="https://www.youtube.com/embed/hkdnz3mQoAg"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen />
-            </div>
-          </WhoWeAreVideo>
-        </WhoWeAre>
-        <JoinTheNetwork>
-          <Img draggable="false" fluid={data.joinTheNetwork.childImageSharp.fluid} />
-          <JoinTheNetworkText>
-            <H1>Join the Network 💡</H1>
-            <P>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet ipsum turpis, sit amet volutpat urna egestas sit amet. Sed id ante at augue ullamcorper tincidunt a sed est. Quisque accumsan mattis massa, sit amet tincidunt erat commodo blandit. Duis aliquam, arcu vel euismod rutrum, urna arcu pellentesque mauris, eget iaculis massa nulla eget est. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut sed erat ullamcorper neque lacinia lacinia eget quis  </P>
-            <Button text="Become a Member" />
-            <div style={{padding: "0.5em"}}></div>
-            <Button text="Join Our Discord" />
-          </JoinTheNetworkText>
-        </JoinTheNetwork>
-        </Content>
-      </BackgroundShading>
+      <Content>
+        <BackgroundShading>
+          <WhoWeAre>
+            <WhoWeAreText>
+              <H1>Who We Are 🚀</H1>
+              <div className="whoWeAreTextContainer">
+                <P>Western Founders Network is a community of <b>creatives and innovators</b>. Wielding the forces of <b>technology, entrepreneurship, and business,</b> our members convert long-term goals into practical and creative plans for action. Our club enables those with a <b>drive</b>, alongside a community of like-minded peers, to have an impact by opening the door to new skillsets and frameworks of thoughts, such as through our educational and flagship conferences.</P>
+              </div>
+              <WhoWeAreButtons>
+                <Button text="Our Initiatives" />
+                <ButtonInverted text="Meet the Team" />
+              </WhoWeAreButtons>
+            </WhoWeAreText>
+            <WhoWeAreVideo>
+              <div className="container">
+                <iframe
+                  className="responsive-iframe"
+                  src="https://www.youtube.com/embed/hkdnz3mQoAg"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen />
+              </div>
+            </WhoWeAreVideo>
+          </WhoWeAre>
+          <JoinTheNetwork>
+            {true && <Img draggable="false" fluid={data.joinTheNetwork.childImageSharp.fluid} />}
+            <JoinTheNetworkText>
+              <H1>Join the Network 💡</H1>
+              <P>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet ipsum turpis, sit amet volutpat urna egestas sit amet. Sed id ante at augue ullamcorper tincidunt a sed est. Quisque accumsan mattis massa, sit amet tincidunt erat commodo blandit. Duis aliquam, arcu vel euismod rutrum, urna arcu pellentesque mauris, eget iaculis massa nulla eget est. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut sed erat ullamcorper neque lacinia lacinia eget quis  </P>
+              <Button text="Become a Member" />
+              <div style={{ padding: "0.5em" }}></div>
+              <Button text="Join Our Discord" />
+            </JoinTheNetworkText>
+          </JoinTheNetwork>
+        </BackgroundShading>
+      </Content>
     </ThemeProvider >
   )
 }
