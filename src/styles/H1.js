a5da9f9bc2be3a1f67026f62b9882bf7c826951a
Jack <jackepeplinski@gmail.com>
2021-01-24 21:51:47 -0700

    Specify bold after importing react-boostrap.

--- src/styles/H1.js
+++ src/styles/H1.js
@@ -4,7 +4,8 @@ const StyledH1 = styled.h1`
     font-size: ${props => props.theme.fontXLarge};
     text-align: ${props => props.center ? "center":"initial"};
     margin: 0;
-    color: ${props => props.white ? "#FFFFFF":"#333333"}
+    color: ${props => props.white ? "#FFFFFF":"#333333"};
+    font-weight: bold;
 `;
 const H1 = ({center, white, children}) => {
     return <StyledH1 center={center} white={white}>{children}</StyledH1>
