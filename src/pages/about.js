29a51dd5f64490ab5749c9e17e994fa69eafa63f
Jack <jackepeplinski@gmail.com>
2021-02-10 16:39:06 -0700

    Background shading.

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import { graphql, useStaticQuery } from "gatsby"
 import Button from "../components/Button"
 import ButtonInverted from "../components/ButtonInverted"
 import BackgroundImage from "../components/BackgroundImage"
-import { Theme, H2, P, device } from "../styles/Global.js"
+import { Theme, H2, P, device, BackgroundShading } from "../styles/Global.js"
 import { WhoWeAre, WhoWeAreText, WhoWeAreVideo, Content } from "./index"
 import H1 from "../styles/H1"
 import Img from "gatsby-image"
@@ -44,34 +44,36 @@ const AboutUsPage = () => {
         <H1 center white>About Us</H1>
         <H2>Technology. Business. Entrepreneurship.</H2>
       </BackgroundImage>
-      
-        <Stats>
-          <StatsItem top="600+" bottom="Members" />
-          <StatsItem top="35+" bottom="Events" />
-          <StatsItem top="20+" bottom="Corporate Partners" />
-          <StatsItem top="1" bottom="Philosophy" />
-        </Stats>
+
+      <Stats>
+        <StatsItem top="600+" bottom="Members" />
+        <StatsItem top="35+" bottom="Events" />
+        <StatsItem top="20+" bottom="Corporate Partners" />
+        <StatsItem top="1" bottom="Philosophy" />
+      </Stats>
+      <BackgroundShading>
         <Content>
-        <WhoWeAre>
-          <WhoWeAreText>
-            <H1>Club Mission 🎯</H1>
-            <P>Founders Network is a community built on the passion of those excited to shape our future. We focus on the following three pillars: Technology, Business, and Entrepreneurship. We are the club that strategizes how to bring you the valuable skills and insider knowledge you need to reach your ideals. The insights, connections, and community available only outside of standard lectures and networking events. Our goal is to ensure that you never leave a WFN event without discovering something that brings you closer to your goals and leaves you hungry for more. Whether you’re mastering your first technology like Python, or seeking to change the fabric of society, Founders Network is the community for you. We are curious, skilled and ready to be at the leading edge of technology and business revolutions.  </P>
-          </WhoWeAreText>
-          <WhoWeAreVideo>
-            <Img draggable="false" fluid={data.clubMission.childImageSharp.fluid} />
-          </WhoWeAreVideo>
-        </WhoWeAre>
-        <WhoWeAre>
-          <WhoWeAreVideo>
-            <Img draggable="false" fluid={data.meetOurTeam.childImageSharp.fluid} />
-          </WhoWeAreVideo>
-          <WhoWeAreText>
-            <H1>Meet Our Team</H1>
-            <P>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lectus nunc id arcu ultricies ut massa nunc. Vulputate leo velit, est dictum et amet quam ac. Malesuada quisque diam consectetur non. Tortor metus, volutpat mauris ultrices imperdiet turpis. Adipiscing malesuada aliquam mauris posuere a, nibh ornare. Ut amet, consectetur sapien enim consequat nam. Neque dignissim morbi feugiat blandit id.
-Id ultricies nunc turpis eleifend pellentesque habitant feugiat mattis elementum. Cras.</P>
-          </WhoWeAreText>
-        </WhoWeAre>
-      </Content>
+          <WhoWeAre>
+            <WhoWeAreText>
+              <H1>Club Mission 🎯</H1>
+              <P>Founders Network is a community built on the passion of those excited to shape our future. We focus on the following three pillars: Technology, Business, and Entrepreneurship. We are the club that strategizes how to bring you the valuable skills and insider knowledge you need to reach your ideals. The insights, connections, and community available only outside of standard lectures and networking events. Our goal is to ensure that you never leave a WFN event without discovering something that brings you closer to your goals and leaves you hungry for more. Whether you’re mastering your first technology like Python, or seeking to change the fabric of society, Founders Network is the community for you. We are curious, skilled and ready to be at the leading edge of technology and business revolutions.  </P>
+            </WhoWeAreText>
+            <WhoWeAreVideo>
+              <Img draggable="false" fluid={data.clubMission.childImageSharp.fluid} />
+            </WhoWeAreVideo>
+          </WhoWeAre>
+          <WhoWeAre>
+            <WhoWeAreVideo>
+              <Img draggable="false" fluid={data.meetOurTeam.childImageSharp.fluid} />
+            </WhoWeAreVideo>
+            <WhoWeAreText>
+              <H1>Meet Our Team</H1>
+              <P>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lectus nunc id arcu ultricies ut massa nunc. Vulputate leo velit, est dictum et amet quam ac. Malesuada quisque diam consectetur non. Tortor metus, volutpat mauris ultrices imperdiet turpis. Adipiscing malesuada aliquam mauris posuere a, nibh ornare. Ut amet, consectetur sapien enim consequat nam. Neque dignissim morbi feugiat blandit id.
+            Id ultricies nunc turpis eleifend pellentesque habitant feugiat mattis elementum. Cras.</P>
+            </WhoWeAreText>
+          </WhoWeAre>
+        </Content>
+      </BackgroundShading>
     </ThemeProvider>
   )
 }
