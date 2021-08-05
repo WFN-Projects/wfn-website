b696dd9729038a3a10ee08c63cbf2309ca76757d
Nicholas-Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2021-08-05 10:55:35 -0400

    Give maxWidth to ContentWrapper

--- src/components/ContentWrapper.js
+++ src/components/ContentWrapper.js
@@ -1,9 +1,9 @@
 import styled from "styled-components"
 
 const ContentWrapper = styled.div`
-  margin-top: 5vh;
-  margin-left: 5vw;
-  margin-right: 5vw;
+  padding: 5vh 5vw 0 5vw;
+  margin: auto;
+  max-width: 1600px;
 `
 
 export default ContentWrapper
