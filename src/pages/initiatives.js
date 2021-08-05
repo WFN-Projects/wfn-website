e54d5884162cec70da38614fb988f33104813e12
Nicholas-Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2021-08-05 13:40:48 -0400

    cleanup code

--- src/pages/initiatives.js
+++ src/pages/initiatives.js
@@ -1,25 +1,21 @@
 import React from "react"
-import { ThemeProvider } from "styled-components"
+import styled, { ThemeProvider } from "styled-components"
 import { graphql, useStaticQuery } from "gatsby"
 import Button from "../components/Button"
-import ButtonInverted from "../components/ButtonInverted"
 import BackgroundImage from "../components/BackgroundImage"
 import { Theme, device, BackgroundShading } from "../styles/Global"
 import H1 from "../styles/H1"
 import H2 from "../styles/H2"
 import P from "../styles/P"
-import TextMediaDiv from "../components/TextMediaDiv"
 import Img from "gatsby-image"
-import styled from "styled-components"
 import "../styles/Global.css"
 import useMedia from "use-media"
-import Image from "../images/squiggleL.png"
-import { Content } from "../pages/index"
+import ContentWrapper from "../components/ContentWrapper"
 
 const InitiativesPage = () => {
     const data = useStaticQuery(graphql`
         query Initiatives {
-            squiggleL: file(relativePath: {eq: "SquiggleL.png"}) {
+            squiggleL: file(relativePath: {eq: "squiggleL.png"}) {
                 childImageSharp {
                     fluid(quality:100) {
                         ...GatsbyImageSharpFluid
@@ -53,33 +49,45 @@ const InitiativesPage = () => {
     const isLargeEnough = useMedia({minWidth: 1440})
 
     return (
-        <ThemeProvider theme={Theme}>
-            <BackgroundShading>
-                <BackgroundImage fluid={data.initiatives.childImageSharp.fluid}>
-                    <H1 center white>Our Initiatives</H1>
-                </BackgroundImage>
-                <div className="random">
-                    {isLargeEnough && <Img className="squiggleL" style={{position:"absolute"}} objectFit="fill" fluid={data.squiggleL.childImageSharp.fluid}/>}
-                    {isLargeEnough && <Img className="squiggleR" style={{position:"absolute"}} objectFit="fill" fluid={data.squiggleR.childImageSharp.fluid}/>}
-                    <Content>
-                        <H1 center>View Our Initiatives</H1>
-                        <ButtonRow>
-                            <Button text="Upcoming" />
-                            <Button text="Flagship" />
-                            <Button text="Community" />
-                        </ButtonRow>
-                        <H1 center>Overview</H1>
-                        <OverviewCard>
-                            <Img className="overview" fluid={data.competitions.childImageSharp.fluid} objectFit="cover" />
-                            <OverviewText>
-                                <H2 center white bold>Competitions</H2>
-                                <P white>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam vel tellus egestas porttitor cum sollicitudin. Eget auctor faucibus sapien, lorem ut adipiscing. Egestas maecenas amet, nam venenatis. Sed vestibulum porta elementum est. Sed vestibulum porta elementum est.Sed vestibulum porta elementum est.Sed vestibulum porta elementum est.Sed vestibulum porta elementum est.Sed vestibulum porta elementum est.Sed vestibulum porta elementum est.</P>
-                            </OverviewText>
-                        </OverviewCard>
-                    </Content>
-                </div>
-            </BackgroundShading>
-        </ThemeProvider>
+      <ThemeProvider theme={Theme}>
+        <BackgroundShading>
+          <BackgroundImage fluid={data.initiatives.childImageSharp.fluid}>
+            <H1 center white>Our Initiatives</H1>
+          </BackgroundImage>
+              {isLargeEnough && 
+                <Img 
+                  className="squiggleL" 
+                  style={{position:"absolute"}} 
+                  objectFit="fill" 
+                  fluid={data.squiggleL.childImageSharp.fluid}
+                />
+              }
+              {isLargeEnough && 
+                <Img 
+                  className="squiggleR" 
+                  style={{position:"absolute"}} 
+                  objectFit="fill" 
+                  fluid={data.squiggleR.childImageSharp.fluid}
+                />
+              }
+            <ContentWrapper>
+                <H1 center>View Our Initiatives</H1>
+                <ButtonRow>
+                  <Button text="Upcoming" />
+                  <Button text="Flagship" />
+                  <Button text="Community" />
+                </ButtonRow>
+                <H1 center>Overview</H1>
+              <OverviewCard>
+                <Img className="overview" fluid={data.competitions.childImageSharp.fluid} objectFit="cover" />
+                <OverviewText>
+                  <H2 center white bold>Competitions</H2>
+                  <P white>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam vel tellus egestas porttitor cum sollicitudin. Eget auctor faucibus sapien, lorem ut adipiscing. Egestas maecenas amet, nam venenatis. Sed vestibulum porta elementum est. Sed vestibulum porta elementum est.Sed vestibulum porta elementum est.Sed vestibulum porta elementum est.Sed vestibulum porta elementum est.Sed vestibulum porta elementum est.Sed vestibulum porta elementum est.</P>
+                </OverviewText>
+              </OverviewCard>
+            </ContentWrapper>
+        </BackgroundShading>
+      </ThemeProvider>
     )
 }
 
