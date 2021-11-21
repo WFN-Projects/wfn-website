3fa32f1817a62794535571b09754dd9954f06428
Samuel Catania <69449284+SamuelCatania@users.noreply.github.com>
2021-11-21 11:02:17 -0500

    Attempt to merge Button.js and ButtonInverted.js into one component. I haven't figured out how to open a test website on my end so this may not work.

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,19 @@ import { Link } from 'gatsby'
 const Button = (props) => {
   const internal = /^\/(?!\/)/.test(props.link ? props.link : '/')
 
-  if (internal)
+  if (props.inverted) {
+    return (
+      <Link to={props.link ? props.link : '/'} style={{ textDecoration: 'none' }}>
+        <Outer>
+          <Inner>
+            <Text>{props.text}</Text>
+          </Inner>
+        </Outer>
+      </Link>
+    )
+  }
+
+  else if (internal) {
     return (
       <Link
         to={props.link ? props.link : '/'}
@@ -14,22 +26,27 @@ const Button = (props) => {
         <StyledButton>{props.text}</StyledButton>
       </Link>
     )
+  }
 
-  return (
-    <a
-      href={props.link ? props.link : '/'}
-      style={{ textDecoration: 'none', color: 'white' }}
-      target='_blank'
-      rel='noreferrer'
-    >
-      <StyledButton>{props.text}</StyledButton>
-    </a>
-  )
+  else {
+    return (
+      <a
+        href={props.link ? props.link : '/'}
+        style={{ textDecoration: 'none', color: 'white' }}
+        target='_blank'
+        rel='noreferrer'
+      >
+        <StyledButton>{props.text}</StyledButton>
+      </a>
+    )
+  }
 }
 
 export default Button
 
-const StyledButton = styled.div`
+if (props.inverted)
+
+  const StyledButton = styled.div`
   min-width: 8em;
   height: 2em;
   border: none;
@@ -43,4 +60,39 @@ const StyledButton = styled.div`
   display: inline-block;
   text-align: center;
   cursor: pointer;
+`
+const Outer = styled.div`
+  min-width: 8em;
+  height: 2em;
+  border: none;
+  background: linear-gradient(160.67deg, #ffe9b3 -3.1%, #c54e9e 77.92%);
+  border-radius: 8px;
+  font-size: ${(props) => props.theme.fontMedium};
+  display: inline-flex;
+  padding: 3px;
+  text-align: center;
+  cursor: pointer;
+  box-sizing: border-box;
+`
+const Inner = styled.div`
+  min-width: 8em;
+  width: 100%;
+  height: 100%;
+  border: none;
+  background-color: white;
+  border-radius: 5px;
+  font-size: ${(props) => props.theme.fontMedium};
+  display: flex;
+  align-items: center;
+  vertical-align: middle;
+  padding-left: 15px;
+  padding-right: 15px;
+  text-align: center;
+  cursor: pointer;
+`
+const Text = styled.div`
+  background: linear-gradient(160.67deg, #ffe9b3 -3.1%, #c54e9e 77.92%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  width: 100%;
 `
