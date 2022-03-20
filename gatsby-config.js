3845e9c021609cb187a6f9430ae6a3334e51583f
Nicholas Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2022-03-20 19:25:02 -0400

    add to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     "gatsby-plugin-offline",
     "gatsby-transformer-sharp",
     "gatsby-plugin-styled-components",
+    `gatsby-plugin-netlify`,
     {
       resolve: "gatsby-source-filesystem",
       options: {
